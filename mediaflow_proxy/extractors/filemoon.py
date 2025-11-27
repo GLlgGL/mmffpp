a65c45ae2021cfc24a6ea600116265e1fb27e2e1
GSTAR <44748406+GLlgGL@users.noreply.github.com>
2025-11-27 14:49:57 +0100

    Update filemoon.py

--- mediaflow_proxy/extractors/filemoon.py
+++ mediaflow_proxy/extractors/filemoon.py
@@ -24,6 +24,12 @@ class FileMoonExtractor(BaseExtractor):
 
         final_url = await eval_solver(self, iframe_url, headers, patterns)
 
+        test_resp = await self._make_request(final_url, headers=headers, method="HEAD")
+
+        if test_resp.status_code == 404:
+            # This tells MediaFlow that the stream is not usable
+            raise ExtractorError("Stream not found (404)")
+
         self.base_headers["referer"] = url
         return {
             "destination_url": final_url,
