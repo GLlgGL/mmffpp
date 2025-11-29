c6aff7429041f2d607f9bbe6a506082d48680b86
GSTAR <44748406+GLlgGL@users.noreply.github.com>
2025-11-29 16:03:21 +0100

    Update factory.py

--- mediaflow_proxy/extractors/factory.py
+++ mediaflow_proxy/extractors/factory.py
@@ -15,7 +15,6 @@ from mediaflow_proxy.extractors.streamtape import StreamtapeExtractor
 from mediaflow_proxy.extractors.supervideo import SupervideoExtractor
 from mediaflow_proxy.extractors.uqload import UqloadExtractor
 from mediaflow_proxy.extractors.vavoo import VavooExtractor
-from mediaflow_proxy.extractors.vidguard import VidGuardExtractor
 from mediaflow_proxy.extractors.vidmoly import VidmolyExtractor
 from mediaflow_proxy.extractors.vidoza import VidozaExtractor
 from mediaflow_proxy.extractors.vixcloud import VixCloudExtractor
